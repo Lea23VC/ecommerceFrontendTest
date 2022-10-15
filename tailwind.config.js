35adf595b1e07e593eaf08e62079a031a7268d50
Lea23VC <leandro.villalobos.dx@gmail.com>
2022-10-15 17:01:17 -0300

    :lipstick: Modify tailwind config and add font awesome
    Add font awesome for html icons

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        headerGreen: "#cae6da",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+};
