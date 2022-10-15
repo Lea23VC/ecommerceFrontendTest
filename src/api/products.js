6718f21c27309c1244385d76bcbe75f88161cc8a
Lea23VC <leandro.villalobos.dx@gmail.com>
2022-10-15 14:15:36 -0300

    :construction: Add products fetch
    Add initial template for products

--- src/api/products.js
+++ src/api/products.js
@@ -1,2 +1,7 @@
-const products = "Something";
-export { products };
+export default async function getProducts() {
+  const url =
+    "https://ecommerce-backend-test-asq4pnttb-lea23vc.vercel.app/products";
+  const response = await fetch(url);
+
+  return await response.json();
+}
--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,9 +1,37 @@
-import { products } from "../api/products";
+import products from "../api/products.js";
 
-const div = document.getElementById("div");
+function loadData() {
+  const element = document.querySelector("div#div");
 
-const paragraph = document.createElement("p");
+  products().then((posts) => {
+    console.log("Posts: ", posts.data);
+    const template = getTemplate(posts.data);
 
-paragraph.textContent = products;
+    element.innerHTML = template;
+  });
+}
 
-div.appendChild(paragraph);
+function getTemplate(posts) {
+  const rows = posts.map(postToRowView).join("");
+  console.log("rows: ", rows);
+
+  return `<table>
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Body</th>
+        </tr>
+      </thead>
+  
+      <tbody>${rows}</tbody>
+    </table>`;
+}
+
+function postToRowView(post) {
+  return `<t>
+      <td>${post.name}</td>
+      <td>${post.price}</td>
+    </tr>`;
+}
+
+loadData();
