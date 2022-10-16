8f6d0a2446912bc3d48c0aa01e71fe381d916acf
Lea23VC <leandro.villalobos.dx@gmail.com>
2022-10-15 21:27:22 -0300

    :lipstick: Style product box
    Add borders, buttons and responsive layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ tailwind.config = {
         headerGreen: "#cae6da",
         headerYellow: "#fef5b9",
         softPink: "rgba(239, 200, 223, 0.5)",
+        pink: "#e83e8c",
       },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
