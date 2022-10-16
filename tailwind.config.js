04ecb85b9b0405338d327f201d4a2304ed62ccd2
Lea23VC <leandro.villalobos.dx@gmail.com>
2022-10-16 10:54:24 -0300

    :children_crossing: Add activity indicator before product fetch

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ tailwind.config = {
         headerYellow: "#fef5b9",
         softPink: "rgba(239, 200, 223, 0.5)",
         pink: "#e83e8c",
+        lightGrey: "#f3f3f3",
       },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
